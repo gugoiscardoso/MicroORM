4d3754b51646a0d7ceb12338c0615ddb88612f77
Gustavo de Gois Cardoso <gu-goiscardoso@hotmail.com>
2017-08-16 02:43:33 -0300

    Adicionar arquivos de projeto.

---
+++ MicroOrm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MicroOrm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
